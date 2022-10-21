87027b0ac066e18d049a013bb37182675c0ff957
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-10-21 17:22:47 -0300

    adding a movie animation to splashscreen

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-
+import LottieView from 'lottie-react-native';
 import Movies from './src/Movies';
 import DetailMovie from './src/DetailMovie';
+import {View, StyleSheet} from 'react-native';
 
 const Stack = createStackNavigator();
+
+const SplashScreen = props => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 5000);
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      props.navigation.replace('Filmes');
+    }
+  }, [loaded, props.navigation]);
+
+  return (
+    <View style={styles.root}>
+      <LottieView
+        source={require('./assets/movie-loading.json')}
+        autoPlay
+        loop
+      />
+    </View>
+  );
+};
 
 function Myapp() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Filmes" component={Movies} />
         <Stack.Screen name="Detalhes do filme" component={DetailMovie} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Myapp;
